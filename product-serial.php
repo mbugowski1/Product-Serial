db3b85d57bac99376de7b94f46dbe2c1ec9e03e0
Michał Bugowski <mbugowski1@wp.pl>
2024-09-12 14:44:09 +0200

    Initial

---
+++ product-serial.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: product-serial
+* Plugin URI: https://www.your-site.com/
+* Description: Product with serial numbers for woocommerce
+* Version: 0.1
+* Author: Michał Bugowski
+* Author URI: https://www.your-site.com/
+**/
+ 
